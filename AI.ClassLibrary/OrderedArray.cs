283a0e3cce44d3575772569636a17b80582e1fcf
kourosh <kourosh23@hotmail.com>
2025-03-25 22:23:50 +0100

    Add project files.

---
+++ AI.ClassLibrary/OrderedArray.cs
@@ -0,0 +1,58 @@
+﻿namespace AI.ClassLibrary
+{
+    public class OrderedArray(int Capacity, int LastPosition = -1)
+    {
+        public int[] Values { get; set; } = new int[Capacity];
+
+        public void Print()
+        {
+            if (LastPosition == -1)
+            {
+                Console.WriteLine("The Array is Empty");
+            }
+            else
+            {
+                for (int i = 0; i <= LastPosition; i++)
+                {
+                    Console.WriteLine($"Pos: {i} - Value: {Values[i]}");
+                }
+            }
+        }
+
+        public void Insert(int value)
+        {
+            if (LastPosition == Capacity - 1)
+            {
+                Console.WriteLine("Max Capacity has been reached");
+                return;
+            }
+
+            int positionForInsert = 0;
+            for (int i = 0; i <= LastPosition; i++)
+            {
+                positionForInsert = i;
+
+                if (Values[i] > value)
+                {
+                    break;
+                }
+
+                if (i == LastPosition)
+                {
+                    positionForInsert = i + 1;
+                }
+            }
+
+            //push right
+            var lastPosition = LastPosition;
+            while (lastPosition >= positionForInsert)
+            {
+                Values[lastPosition + 1] = Values[lastPosition];
+                lastPosition = lastPosition - 1;
+            }
+
+            Values[positionForInsert] = value;
+            LastPosition = LastPosition + 1;
+        }
+    }
+}
---
+++ AI.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using AI.ClassLibrary;
+
+var oa = new OrderedArray(10);
+oa.Print();
+
+oa.Insert(6);
+oa.Insert(4);
+oa.Insert(7);
+
+oa.Print();
